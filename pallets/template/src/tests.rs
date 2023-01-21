e202c2bfc176da24cc3b77b69b653d066aafffab
Achim Schneider <achim@parity.io>
2023-01-21 13:25:48 +0100

    ci: fix benchmarking command

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -2,15 +2,142 @@ use crate::mock::*;
 use frame_support::assert_ok;
 
 #[test]
-fn groth16_verification_works() {
+fn groth16_optimized_verification() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::verify_groth16(RuntimeOrigin::signed(1)));
+		assert_ok!(TemplateModule::verify_groth16_optimized(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn pairing_arkworks_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::pairing_arkworks_bls12_381(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn msm_g1_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::msm_g1_bls12_381(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn msm_g2_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::msm_g2_bls12_381(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_affine_g1_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_affine_g1_bls12_381(RuntimeOrigin::signed(1)));
 	});
 }
 
 #[test]
-fn groth16_optimized_verification_works() {
+fn mul_projective_g1_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_projective_g1_bls12_381(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_affine_g2_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_affine_g2_bls12_381(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_projective_g2_bls12_381_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_projective_g2_bls12_381(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_affine_g1_bls12_377_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_affine_g1_bls12_377(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_projective_g1_bls12_377_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_projective_g1_bls12_377(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_affine_g2_bls12_377_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_affine_g2_bls12_377(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_projective_g2_bls12_377_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_projective_g2_bls12_377(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn pairing_arkworks_bls12_377_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::pairing_arkworks_bls12_377(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn msm_g2_bw6_761_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::msm_g2_bw6_761(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_affine_g1_bw6_761_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_affine_g1_bw6_761(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_projective_g1_bw6_761_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_projective_g1_bw6_761(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_affine_g2_bw6_761_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_affine_g2_bw6_761(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn mul_projective_g2_bw6_761_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::mul_projective_g2_bw6_761(RuntimeOrigin::signed(1)));
+	});
+}
+#[test]
+fn pairing_arkworks_bw6_761_works_optimized() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::pairing_arkworks_bw6_761(RuntimeOrigin::signed(1)));
+	});
+}
+
+#[test]
+fn groth16_optimized_verification() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
 		assert_ok!(TemplateModule::verify_groth16_optimized(RuntimeOrigin::signed(1)));
@@ -143,3 +270,4 @@ fn pairing_arkworks_bw6_761_works() {
 		assert_ok!(TemplateModule::pairing_arkworks_bw6_761(RuntimeOrigin::signed(1)));
 	});
 }
+
