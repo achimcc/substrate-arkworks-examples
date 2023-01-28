aa8f4f1aa1e7d5a64c8dceec4230868f1e198e36
Achim Schneider <achim@parity.io>
2023-01-28 12:58:49 +0100

    algebra bench tamplates: bls_377

--- pallets/template/benches/bls12_377.rs
+++ pallets/template/benches/bls12_377.rs
@@ -1,6 +1,28 @@
 use ark_algebra_bench_templates::*;
 use criterion::Criterion;
 
+bench!(
+	Name = "Bls12_377",
+	Pairing = ark_bls12_377::Bls12_381,
+	G1 = ark_bls12_377::G1Projective,
+	G2 = ark_bls12_377::G2Projective,
+	ScalarField = ark_bls12_377::fr::Fr,
+	G1BaseField = ark_bls12_377::fq::Fq,
+	G2BaseField = ark_bls12_377::fq2::Fq2,
+	TargetField = ark_bls12_377::Fq12,
+);
+
+bench!(
+	Name = "Bls12_381_optimied",
+	Pairing = pallet_template::bls12_377::Bls12_381_optimied,
+	G1 = pallet_template::bls12_377::G1ProjectiveBls12_377_Host
+	G2 = pallet_template::bls12_377::G2ProjectiveBls12_377_Host,
+	ScalarField = sp_ark_bls12_377::fr::Fr,
+	G1BaseField = sp_ark_bls12_377::bls12_381::fq::Fq,
+	G2BaseField = sp_ark_bls12_377::bls12_381::fq2::Fq2,
+	TargetField = sp_ark_bls12_377::bls12_381::Fq12,
+);
+
 pub fn bench_pairing_bls12_377(c: &mut Criterion) {
 	let mut group = c.benchmark_group("pairing_bls12_377");
 	group.bench_function("normal", |b| {
