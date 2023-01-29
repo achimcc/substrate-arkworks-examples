dfa95e951fbe0ff05fb1e56515d0d3dd1e63132d
Achim Schneider <achim@parity.io>
2023-01-29 11:24:13 +0100

    fix clippy

--- pallets/template/benches/bw6_761_template_optimized.rs
+++ pallets/template/benches/bw6_761_template_optimized.rs
@@ -2,7 +2,7 @@ use ark_algebra_bench_templates::*;
 use pallet_template::bw6_761::{BW6_761Optimized, G1ProjectiveBW6_761, G2ProjectiveBW6_761};
 use sp_ark_bw6_761::{
 	fq::Fq as FqOptimized, fq3::Fq3 as Fq3Optimized, fq6::Fq6 as Fq6Optimized,
-	fr::Fr as Fr_optimized,
+	fr::Fr as FrOptimized,
 };
 
 bench!(
