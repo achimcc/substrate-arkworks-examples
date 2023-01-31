336d84ba7d5b1ff48e4a2a0e113273a77006a21c
Achim Schneider <achim@parity.io>
2023-01-31 11:37:50 +0100

    fmt

--- pallets/template/src/bw6_761.rs
+++ pallets/template/src/bw6_761.rs
@@ -108,7 +108,6 @@ pub fn do_mul_projective_g1_optimized() -> Result<(), Error> {
 	Ok(())
 }
 
-
 pub fn do_mul_affine_g2() -> Result<(), Error> {
 	let _out = <ark_bw6_761::g2::Config as SWCurveConfig>::mul_affine(
 		&ark_bw6_761::G2Affine::generator(),
