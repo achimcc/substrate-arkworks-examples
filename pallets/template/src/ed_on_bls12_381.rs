aee005c9cea57b67b19f7dab170e02925e4b7e70
Achim Schneider <achim@parity.io>
2023-03-02 22:17:43 +0100

    Update ed_on_bls12_381.rs

--- pallets/template/src/ed_on_bls12_381.rs
+++ pallets/template/src/ed_on_bls12_381.rs
@@ -25,45 +25,45 @@ impl EdwardBls12_381HostFunctions for HostEdOnBls12_381 {
 	}
 }
 
-pub fn do_msm_sw(samples: u32) -> Result<(), Error> {
-	let g = ark_ed_on_bls12_381::SWAffine::generator();
-	let v: Vec<_> = (0..samples).map(|_| g).collect();
-	let scalars: Vec<_> = (0..samples).map(|_| ark_ff::Fp::from(2u32)).collect();
-	let _out = <ark_ed_on_bls12_381::EdwardsConfig as SWCurveConfig>::msm(&v[..], &scalars[..]);
+pub fn do_msm_sw(
+	bases: &[sp_ark_models::short_weierstrass::Affine<ark_ed_on_bls12_381::SWConfig>],
+	scalars: &[<ark_ed_on_bls12_381::SWConfig as ark_ec::CurveConfig>::ScalarField],
+) -> Result<(), Error> {
+	let _out = <ark_ed_on_bls12_381::EdwardsConfig as SWCurveConfig>::msm(bases, scalars);
 	Ok(())
 }
 
-pub fn do_msm_sw_optimized(samples: u32) -> Result<(), Error> {
-	let g = ark_ec::short_weierstrass::Affine::<
-		sp_ark_ed_on_bls12_381::JubjubConfig<HostEdOnBls12_381>,
-	>::generator();
-	let v: Vec<_> = (0..samples).map(|_| g).collect();
-	let scalars: Vec<_> = (0..samples).map(|_| ark_ff::Fp::from(2u64)).collect();
+pub fn do_msm_sw_optimized(
+	bases: &[sp_ark_models::short_weierstrass::Affine<
+		sp_ark_ed_on_bls12_381::SWConfig<HostEdOnBls12_381>,
+	>],
+	scalars: &[<sp_ark_ed_on_bls12_381::SWConfig<HostEdOnBls12_381> as sp_ark_models::CurveConfig>::ScalarField],
+) -> Result<(), Error> {
 	let _out = <sp_ark_ed_on_bls12_381::EdwardsConfig<HostEdOnBls12_381> as SWCurveConfig>::msm(
-		&v[..],
-		&scalars[..],
+		bases, scalars,
 	);
 	Ok(())
 }
 
-pub fn do_msm_te(samples: u32) -> Result<(), Error> {
-	let g = ark_ed_on_bls12_381::EdwardsAffine::generator();
-	let v: Vec<_> = (0..samples).map(|_| g).collect();
-	let scalars: Vec<_> = (0..samples).map(|_| ark_ff::Fp::from(2u64)).collect();
+pub fn do_msm_te(
+	bases: &[ark_ec::twisted_edwards::Affine<ark_ed_on_bls12_381::JubjubConfig>],
+	scalars: &[<ark_ed_on_bls12_381::EdwardsConfig as ark_ec::CurveConfig>::ScalarField],
+) -> Result<(), Error> {
 	let _out = <ark_ed_on_bls12_381::JubjubConfig as ark_ec::twisted_edwards::TECurveConfig>::msm(
-		&v[..],
-		&scalars[..],
+		bases, scalars,
 	);
 	Ok(())
 }
 
-pub fn do_msm_te_optimized(samples: u32) -> Result<(), Error> {
-	let g = sp_ark_ed_on_bls12_381::EdwardsAffine::<HostEdOnBls12_381>::generator();
-	let v: Vec<_> = (0..samples).map(|_| g).collect();
-	let scalars: Vec<_> = (0..samples).map(|_| ark_ff::Fp::from(2u64)).collect();
+pub fn do_msm_te_optimized(
+	bases: &[sp_ark_models::twisted_edwards::Affine<
+		sp_ark_ed_on_bls12_381::EdwardsConfig<HostEdOnBls12_381>,
+	>],
+	scalars: &[<sp_ark_ed_on_bls12_381::EdwardsConfig<HostEdOnBls12_381> as ark_ec::CurveConfig>::ScalarField],
+) -> Result<(), Error> {
 	let _out = <sp_ark_ed_on_bls12_381::JubjubConfig<HostEdOnBls12_381> as sp_ark_models::twisted_edwards::TECurveConfig>::msm(
-		&v[..],
-		&scalars[..],
+		bases,
+		scalars,
 	);
 	Ok(())
 }
