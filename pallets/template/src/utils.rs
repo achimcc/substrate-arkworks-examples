688a30658a850826b184bc3afbb5ceb785525fdb
Achim Schneider <achim@parity.io>
2023-03-03 13:35:29 +0100

    fix clippy

--- pallets/template/src/utils.rs
+++ pallets/template/src/utils.rs
@@ -1,9 +1,29 @@
 use ark_serialize::{CanonicalSerialize, Compress};
-use ark_std::{io::Cursor, vec, vec::Vec};
+use ark_std::{io::Cursor, test_rng, vec, vec::Vec, UniformRand};
 
 pub fn serialize_argument(argument: impl CanonicalSerialize) -> Vec<u8> {
 	let mut serialized_argument = vec![0u8; argument.serialized_size(Compress::No)];
 	let mut cursor = Cursor::new(&mut serialized_argument[..]);
 	argument.serialize_uncompressed(&mut cursor).unwrap();
 	serialized_argument
+}
+
+pub fn generate_arguments_sw<Group: ark_ec::VariableBaseMSM>(
+	size: u32,
+) -> (Vec<Group>, Vec<Group::ScalarField>) {
+	let rng = &mut test_rng();
+	let scalars = (0..size).map(|_| Group::ScalarField::rand(rng)).collect::<Vec<_>>();
+	let bases = (0..size).map(|_| Group::rand(rng)).collect::<Vec<_>>();
+	(bases, scalars)
+}
+
+pub fn generate_arguments_te<Field: ark_ec::models::twisted_edwards::TECurveConfig>(
+	size: u32,
+) -> (Vec<ark_ec::twisted_edwards::Affine<Field>>, Vec<Field::ScalarField>) {
+	let rng = &mut test_rng();
+	let scalars = (0..size).map(|_| Field::ScalarField::rand(rng)).collect::<Vec<_>>();
+	let bases = (0..size)
+		.map(|_| ark_ec::twisted_edwards::Affine::<Field>::rand(rng))
+		.collect::<Vec<_>>();
+	(bases, scalars)
 }
