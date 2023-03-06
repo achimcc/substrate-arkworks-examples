1731a7c11c9d61745f7cb9ccb33406fa09eb2c57
Achim Schneider <achim@parity.io>
2023-03-06 17:20:47 +0100

    fix dependencies

--- pallets/template/benches/ed_on_bls12_377_template.rs
+++
@@ -1,4 +0,0 @@
-use ark_algebra_bench_templates::*;
-use ark_ed_on_bls12_377::{fq::Fq, EdwardsProjective as G, Fr};
-
-bench!(Name = "EdOnBls12_377", Group = G, ScalarField = Fr, PrimeBaseField = Fq,);
