101a113971c7f46f8061760e4a5baf3008bb455d
Davide Galassi <davxy@datawok.net>
2023-09-25 18:39:26 +0200

    Utils cleanup

--- pallets/ark/src/utils.rs
+++ pallets/ark/src/utils.rs
@@ -15,10 +15,39 @@ pub type ProofFor<PairingT, PrimeFieldT> = <Groth16<PairingT> as SNARK<PrimeFiel
 
 pub type ScalarFieldFor<AffineT> = <AffineT as AffineRepr>::ScalarField;
 
-pub fn serialize_argument(argument: impl CanonicalSerialize) -> Vec<u8> {
-	let mut buf = vec![0; argument.serialized_size(Compress::No)];
-	argument.serialize_uncompressed(&mut buf.as_mut_slice()).unwrap();
-	buf
+// `words_count` is the scalar length in words, with 1 word assumed to be 64 bits.
+// Most significant bit is set.
+fn generate_scalar(words_count: u32) -> Vec<u64> {
+	let mut scalar: Vec<_> =
+		(0..words_count as usize).map(|_| u64::rand(&mut test_rng())).collect();
+	// Arkworks assumes scalar to be in **big endian**
+	scalar[0] |= 1 << 63;
+	scalar
+}
+
+fn generate_base<Group: UniformRand>() -> Group {
+	Group::rand(&mut test_rng())
+}
+
+// `words_count` is the scalar length in words, with 1 word assumed to be 64 bits.
+// Most significant bit is set.
+pub fn generate_scalar_args<Group: UniformRand>(
+	words_count: u32,
+) -> (ArkScale<Group>, ArkScale<Vec<u64>>) {
+	(generate_base::<Group>().into(), generate_scalar(words_count).into())
+}
+
+// `words_count` is the scalar length in words, with 1 word assumed to be 64 bits.
+// Most significant bit is set.
+pub fn generate_scalar_args_projective<Group: UniformRand>(
+	words_count: u32,
+) -> (ArkScaleProjective<Group>, ArkScale<Vec<u64>>) {
+	(generate_base::<Group>().into(), generate_scalar(words_count).into())
+}
+
+pub fn generate_pairing_args<GroupA: UniformRand, GroupB: UniformRand>(
+) -> (ArkScale<GroupA>, ArkScale<GroupB>) {
+	(generate_base::<GroupA>().into(), generate_base::<GroupB>().into())
 }
 
 pub fn generate_msm_args<Group: ark_ec::VariableBaseMSM>(
@@ -32,57 +61,8 @@ pub fn generate_msm_args<Group: ark_ec::VariableBaseMSM>(
 	(bases, scalars)
 }
 
-// `words_count` is the scalar length in words, with 1 word assumed to be 64 bits.
-// Most significant bit is set.
-fn generate_scalar(words_count: u32) -> ArkScale<Vec<u64>> {
-	let rng = &mut test_rng();
-	let mut scalar: Vec<_> = (0..words_count as usize).map(|_| u64::rand(rng)).collect();
-	// Arkworks assumes scalar to be in **big endian**
-	scalar[0] |= 1 << 63;
-	let scalars: ArkScale<Vec<u64>> = scalar.into();
-	scalars
-}
-
-fn generate_base<Group: CanonicalSerialize + UniformRand>() -> ArkScale<Group> {
-	let rng = &mut test_rng();
-	let base = Group::rand(rng);
-	let base: ArkScale<Group> = base.into();
-	base
-}
-
-fn generate_base_projective<Group: CanonicalSerialize + UniformRand>() -> ArkScaleProjective<Group>
-{
-	let rng = &mut test_rng();
-	let base = Group::rand(rng);
-	let base: ArkScaleProjective<Group> = base.into();
-	base
-}
-
-// `words_count` is the scalar length in words, with 1 word assumed to be 64 bits.
-// Most significant bit is set.
-pub fn generate_scalar_args<Group: CanonicalSerialize + UniformRand>(
-	words_count: u32,
-) -> (ArkScale<Group>, ArkScale<Vec<u64>>) {
-	let base = generate_base::<Group>();
-	let scalar = generate_scalar(words_count);
-	(base, scalar)
-}
-
-// `words_count` is the scalar length in words, with 1 word assumed to be 64 bits.
-// Most significant bit is set.
-pub fn generate_scalar_args_projective<Group: CanonicalSerialize + UniformRand>(
-	words_count: u32,
-) -> (ArkScaleProjective<Group>, ArkScale<Vec<u64>>) {
-	let base = generate_base_projective::<Group>();
-	let scalar = generate_scalar(words_count);
-	(base, scalar)
-}
-
-pub fn generate_pairing_args<
-	GroupA: CanonicalSerialize + UniformRand,
-	GroupB: CanonicalSerialize + UniformRand,
->() -> (ArkScale<GroupA>, ArkScale<GroupB>) {
-	let a = generate_base::<GroupA>();
-	let b = generate_base::<GroupB>();
-	(a, b)
+pub fn serialize_argument(argument: impl CanonicalSerialize) -> Vec<u8> {
+	let mut buf = vec![0; argument.serialized_size(Compress::No)];
+	argument.serialize_uncompressed(&mut buf.as_mut_slice()).unwrap();
+	buf
 }
