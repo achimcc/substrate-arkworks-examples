12d1575bc293adcc435bce453e9f645c74c5ad9a
Davide Galassi <davxy@datawok.net>
2023-09-26 17:46:52 +0200

    Remove direct dependency from sp-ark-models

--- pallets/ark/src/bw6_761.rs
+++ pallets/ark/src/bw6_761.rs
@@ -1,5 +1,6 @@
+use crate::utils::ScalarFieldFor;
 use ark_bw6_761::{G1Affine, G1Projective, G2Affine, G2Projective, BW6_761};
-use sp_ark_models::{pairing::Pairing, short_weierstrass::SWCurveConfig};
+use ark_ec::{pairing::Pairing, short_weierstrass::SWCurveConfig};
 use sp_bw6_761::curves::{
 	g1::{G1Affine as G1AffineOpt, G1Projective as G1ProjectiveOpt},
 	g2::{G2Affine as G2AffineOpt, G2Projective as G2ProjectiveOpt},
@@ -14,42 +15,27 @@ pub fn pairing_opt(a: G1AffineOpt, b: G2AffineOpt) {
 	let _out = BW6_761Opt::multi_pairing([a], [b]);
 }
 
-pub fn msm_g1(
-	bases: &[ark_ec::short_weierstrass::Affine<ark_bw6_761::g1::Config>],
-	scalars: &[<ark_bw6_761::g1::Config as ark_ec::CurveConfig>::ScalarField],
-) {
+pub fn msm_g1(bases: &[G1Affine], scalars: &[ScalarFieldFor<G1Affine>]) {
 	let _out = <ark_bw6_761::g1::Config as SWCurveConfig>::msm(bases, scalars);
 }
 
-pub fn msm_g1_opt(
-	bases: &[sp_bw6_761::g1::G1Affine],
-	scalars: &[<sp_bw6_761::g1::Config as sp_ark_models::CurveConfig>::ScalarField],
-) {
+pub fn msm_g1_opt(bases: &[G1AffineOpt], scalars: &[ScalarFieldFor<G1AffineOpt>]) {
 	let _out = <sp_bw6_761::g1::Config as SWCurveConfig>::msm(bases, scalars);
 }
 
-pub fn msm_g2(
-	bases: &[ark_ec::models::short_weierstrass::Affine<ark_bw6_761::g2::Config>],
-	scalars: &[<ark_bw6_761::g2::Config as ark_ec::CurveConfig>::ScalarField],
-) {
+pub fn msm_g2(bases: &[G2Affine], scalars: &[ScalarFieldFor<G2Affine>]) {
 	let _out = <ark_bw6_761::g2::Config as SWCurveConfig>::msm(bases, scalars);
 }
 
-pub fn msm_g2_opt(
-	bases: &[sp_ark_models::short_weierstrass::Affine<sp_bw6_761::g2::Config>],
-	scalars: &[<sp_bw6_761::g2::Config as sp_ark_models::CurveConfig>::ScalarField],
-) {
+pub fn msm_g2_opt(bases: &[G2AffineOpt], scalars: &[ScalarFieldFor<G2AffineOpt>]) {
 	let _out = <sp_bw6_761::g2::Config as SWCurveConfig>::msm(bases, scalars);
 }
 
-pub fn mul_affine_g1(
-	base: &ark_ec::short_weierstrass::Affine<ark_bw6_761::g1::Config>,
-	scalar: &[u64],
-) {
+pub fn mul_affine_g1(base: &G1Affine, scalar: &[u64]) {
 	let _out = <ark_bw6_761::g1::Config as SWCurveConfig>::mul_affine(base, scalar);
 }
 
-pub fn mul_affine_g1_opt(base: &sp_bw6_761::g1::G1Affine, scalar: &[u64]) {
+pub fn mul_affine_g1_opt(base: &G1AffineOpt, scalar: &[u64]) {
 	let _out = <sp_bw6_761::g1::Config as SWCurveConfig>::mul_affine(base, scalar);
 }
 
@@ -61,17 +47,11 @@ pub fn mul_projective_g1_opt(base: &G1ProjectiveOpt, scalar: &[u64]) {
 	let _out = <sp_bw6_761::g1::Config as SWCurveConfig>::mul_projective(base, scalar);
 }
 
-pub fn mul_affine_g2(
-	base: &ark_ec::short_weierstrass::Affine<ark_bw6_761::g2::Config>,
-	scalar: &[u64],
-) {
+pub fn mul_affine_g2(base: &G2Affine, scalar: &[u64]) {
 	let _out = <ark_bw6_761::g2::Config as SWCurveConfig>::mul_affine(base, scalar);
 }
 
-pub fn mul_affine_g2_opt(
-	base: &sp_ark_models::short_weierstrass::Affine<sp_bw6_761::g2::Config>,
-	scalar: &[u64],
-) {
+pub fn mul_affine_g2_opt(base: &G2AffineOpt, scalar: &[u64]) {
 	let _out = <sp_bw6_761::g2::Config as SWCurveConfig>::mul_affine(base, scalar);
 }
 
--- pallets/ark/src/ed_on_bls12_377.rs
+++ pallets/ark/src/ed_on_bls12_377.rs
@@ -1,33 +1,23 @@
 use crate::utils::ScalarFieldFor;
+use ark_ec::twisted_edwards::TECurveConfig;
 use ark_ed_on_bls12_377::EdwardsAffine;
-use sp_ark_models::TECurveConfig;
 pub use sp_ed_on_bls12_377::curves::{
-	EdwardsAffine as EdwardsAffineOptimized, EdwardsProjective as EdwardsProjectiveOptimized,
+	EdwardsAffine as EdwardsAffineOpt, EdwardsProjective as EdwardsProjectiveOpt,
 };
 
 pub fn msm(bases: &[EdwardsAffine], scalars: &[ScalarFieldFor<EdwardsAffine>]) {
-	let _out =
-		<ark_ed_on_bls12_377::EdwardsConfig as ark_ec::models::twisted_edwards::TECurveConfig>::msm(
-			bases, scalars,
-		);
+	let _out = <ark_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::msm(bases, scalars);
 }
 
-pub fn msm_opt(
-	bases: &[EdwardsAffineOptimized],
-	scalars: &[<sp_ed_on_bls12_377::EdwardsConfig as ark_ec::CurveConfig>::ScalarField],
-) {
+pub fn msm_opt(bases: &[EdwardsAffineOpt], scalars: &[ScalarFieldFor<EdwardsAffineOpt>]) {
 	let _out = <sp_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::msm(bases, scalars);
 }
 
 pub fn mul_affine(base: &ark_ed_on_bls12_377::EdwardsAffine, scalar: &[u64]) {
-	let _out =
-		<ark_ed_on_bls12_377::EdwardsConfig as ark_ec::models::twisted_edwards::TECurveConfig>::mul_affine(
-			base,
-			scalar,
-		);
+	let _out = <ark_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::mul_affine(base, scalar);
 }
 
-pub fn mul_affine_opt(base: &EdwardsAffineOptimized, scalar: &[u64]) {
+pub fn mul_affine_opt(base: &EdwardsAffineOpt, scalar: &[u64]) {
 	let _out = <sp_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::mul_affine(base, scalar);
 }
 
@@ -35,6 +25,6 @@ pub fn mul_projective(base: &ark_ed_on_bls12_377::EdwardsProjective, scalar: &[u
 	let _out = <ark_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::mul_projective(base, scalar);
 }
 
-pub fn mul_projective_opt(base: &EdwardsProjectiveOptimized, scalar: &[u64]) {
+pub fn mul_projective_opt(base: &EdwardsProjectiveOpt, scalar: &[u64]) {
 	let _out = <sp_ed_on_bls12_377::EdwardsConfig as TECurveConfig>::mul_projective(base, scalar);
 }
